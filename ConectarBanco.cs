2fe1c2334e7ae3d112071d58d2c1de3a73df96d7
Wallace Alonso <97135071+WallaceAlonsoCarvalho@users.noreply.github.com>
2023-04-02 23:29:15 -0300

    Add files via upload

---
+++ ConectarBanco.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+
+namespace Crud2
+{
+    public class ConectarBanco
+    {
+
+        public static string ConnStr = @"Data Source=192.168.15.175;Integrated Security=True";
+
+        public static SqlConnection GetConnection()
+        {
+            var cn =new SqlConnection(ConnStr);
+
+            return cn;
+        }
+    }
+}
---
+++ ContatoModelcs.cs
@@ -0,0 +1,14 @@
+﻿namespace Crud2.Models
+{
+    public class ContatoModelcs
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public string Site { get; set; }
+
+        public string Segmento { get; set; }
+    }
+}
